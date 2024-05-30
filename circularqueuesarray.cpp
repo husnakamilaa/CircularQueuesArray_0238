76a4884dfabb277994e9dc75825643bd887f34e1
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-05-30 14:55:30 +0700

    membuat decision making untuk ngecek apakah antrian kosong

--- circularqueuesarray.cpp
+++ circularqueuesarray.cpp
@@ -24,5 +24,11 @@ public :
             cout << "\nQueue overflow\n";
             return;
         }
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
     }
 };
